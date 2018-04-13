abec80ee9e357f98a978d59014a9ccf30a094739
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-04-13 11:16:29 +0530

    mocha and bower updated version

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@
 echo "================= Updating global nodejs packages ==================="
 npm install -g \
   grunt-cli@1.2.0 \
-  mocha@4.0.1 \
+  mocha@5.0.5 \
   vows@0.8.1 \
   casperjs@1.1.4 \
-  bower@1.8.2
+  bower@1.8.4
 
 
 for file in /u16nodall/version/*;
