0e571c570ec80ba7397e991f69850645357b7cae
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-06-22 14:35:18 +0530

    Update _node.sh

--- test/_node.sh
+++ test/_node.sh
@@ -1,18 +1,18 @@
 #! /bin/bash -e
 
 
-echo "nvm use 6.14.2"
-nvm use 6.14.2
+echo "nvm use 6.14.3"
+nvm use 6.14.3
 which node && node -v
 printf "\n"
 
-echo "nvm use 8.11.2"
-nvm use 8.11.2
+echo "nvm use 8.11.3"
+nvm use 8.11.3
 which node && node -v
 printf "\n"
 
-echo "nvm use 9.11.1"
-nvm use 9.11.1
+echo "nvm use 9.11.2"
+nvm use 9.11.2
 which node && node -v
 printf "\n"
 
